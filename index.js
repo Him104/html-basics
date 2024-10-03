ea812e6f9a0fea20a7aea0a41445274ef29254d2
himanshu <himanshuchauhan696@gmail.com>
2024-10-03 21:52:43 +0530

    javascript history, data types, primitive and non primitive

---
+++ index.js
@@ -0,0 +1,35 @@
+// let a = 10;
+
+// let b = 20;
+
+// c = a + b;
+
+// console.log(c)
+
+// let a = "anything"
+
+// console.log(typeof a)
+
+
+// const a = true;
+
+// console.log( a)
+
+// let a; //declaration
+
+// a = 50;
+
+// console.log(a)
+
+
+
+// let anonymous = null;
+
+// console.log(typeof anonymous)
+
+
+let arr = [1,2,3,4,5,6,7,8,9,12,13,15]
+
+console.log(arr[4])
+
+
