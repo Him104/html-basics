53d623a8c019bcceb6b20028ab1ecdc4d6f6018b
himanshu <himanshuchauhan696@gmail.com>
2024-10-09 21:48:10 +0530

    adding ternary operators, arithmetic, logical and comparision

---
+++ Javascript/ternary.js
@@ -0,0 +1,16 @@
+// const age = 18;
+
+// const canVote = age != 17 ? true : false;
+
+// console.log(canVote)
+
+const age = 18;
+
+if (age<=17 || age >=20) {
+
+    console.log("yes, you are correct")
+    
+}
+else {
+    console.log("this doesn't falls under and property rules")
+}
