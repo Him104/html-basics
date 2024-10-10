bae6d97245ebd6f4dae36da5c7fb5e9525779d2f
himanshu <himanshuchauhan696@gmail.com>
2024-10-10 21:56:18 +0530

    var, let and const, reassignment, hoisting, scope

---
+++ Javascript/scope.js
@@ -0,0 +1,38 @@
+// var a = 20;
+
+
+// function letExample() {
+//     let x = 10;
+
+
+  
+//     // let x = 20;
+//     // console.log(x);
+  
+// }
+
+// console.log(x);
+
+// letExample();
+
+// const scroll = "titan"
+
+
+// console.log(scroll)
+
+
+console.log(z);
+
+var z = 5;
+
+console.log(z)
+
+
+var x;
+
+console.log(x)
+x = 10;
+
+console.log(x)
+
+
